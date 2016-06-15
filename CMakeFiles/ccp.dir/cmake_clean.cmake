c636b1864387d9604a98d42ea6c60563a8c7b8e9
Mateusz Gawino <mateusz.gawino@gmail.com>
2016-06-16 01:21:17 +0200

    Initial commit

---
+++ CMakeFiles/ccp.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ccp.dir/ccp.c.o"
+  "ccp.pdb"
+  "ccp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ccp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
